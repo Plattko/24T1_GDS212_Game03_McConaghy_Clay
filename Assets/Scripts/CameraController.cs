f96b2d35b44475fcd76fce54309d4e0d2b7d8390
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-02-29 18:30:12 +1100

    Updated Camera to Disconnect From Player
    - Moved distant hills starting positions in Game scene.
    - Updated CameraController script to disconnect and reconnect to the player when panning out/in.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,26 +9,45 @@ namespace Plattko
     {
         private CinemachineVirtualCamera virtualCam;
         private CinemachineFramingTransposer framingTransposer;
+        private Transform followTarget;
 
         private Vector3 startOffset;
         private Vector3 panOutOffset = new Vector3(0f, 5f, 0f);
         private Vector3 velocity = Vector3.zero;
         private float smoothTime = 2f;
 
+        private bool isPannedOut = false;
+
         private void Start()
         {
             virtualCam = GetComponent<CinemachineVirtualCamera>();
             framingTransposer = virtualCam.GetCinemachineComponent<CinemachineFramingTransposer>();
+            followTarget = virtualCam.Follow;
+
             startOffset = framingTransposer.m_TrackedObjectOffset;
         }
 
         public void PanOut()
         {
             framingTransposer.m_TrackedObjectOffset = Vector3.SmoothDamp(framingTransposer.m_TrackedObjectOffset, panOutOffset, ref velocity, smoothTime);
+
+            if (Vector3.Distance(framingTransposer.m_TrackedObjectOffset, panOutOffset) < 0.01f)
+            {
+                virtualCam.Follow = null;
+                isPannedOut = true;
+            }
         }
 
         public void PanIn()
         {
+            if (isPannedOut)
+            {
+                float yTrackingOffset = virtualCam.transform.position.y - followTarget.position.y;
+                framingTransposer.m_TrackedObjectOffset = new Vector3(framingTransposer.m_TrackedObjectOffset.x, yTrackingOffset, framingTransposer.m_TrackedObjectOffset.z);
+                virtualCam.Follow = followTarget;
+                isPannedOut = false;
+            }
+
             framingTransposer.m_TrackedObjectOffset = Vector3.SmoothDamp(framingTransposer.m_TrackedObjectOffset, startOffset, ref velocity, smoothTime);
         }
     }
